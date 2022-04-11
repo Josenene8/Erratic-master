586cc47b5f034b6cb017144fd1a26816b7f2d698
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-04-10 21:38:20 -0300

    Update DialogueBox.hx

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -187,7 +187,19 @@ class DialogueBox extends FlxSpriteGroup
 			startDialogue();
 			dialogueStarted = true;
 		}
+                #if mobile
+		var justTouched:Bool = false;
 
+		for (touch in FlxG.touches.list)
+		{
+			justTouched = false;
+			
+			if (touch.justReleased){
+				justTouched = true;
+			}
+		}
+		#end
+			
 		if (PlayerSettings.player1.controls.ACCEPT && dialogueStarted == true)
 		{
 			remove(dialogue);
