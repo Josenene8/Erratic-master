17d51b22952b1763c2704e5c65faad996337e41a
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-04-19 13:50:25 -0300

    Add files via upload

--- source/Main.hx
+++ source/Main.hx
@@ -72,13 +72,21 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
+		#if cpp
+		initialState = Caching;
+		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
+		#else
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 		#end
 		addChild(game);
-		
+
+		var ourSource:String = "assets/videos/DO NOT DELETE OR GAME WILL CRASH/dontDelete.webm";
+
+		#if !mobile
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsCounter);
 		toggleFPS(FlxG.save.data.fps);
+		#end
 	}
 
 	var game:FlxGame;
